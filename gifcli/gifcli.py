7b996fa5aa2abbc35bddd7b26c3851f68113eaa2
Tom Wardill <tom@howrandom.net>
2018-10-02 16:42:38 +0100

    Fix oops

--- gifcli/gifcli.py
+++ gifcli/gifcli.py
@@ -14,6 +14,7 @@ def get_config():
     config_path = os.path.join(config_dir, 'gif-cli.json')
     with open(config_path) as config_file:
         config = json.load(config_file)
+        return config
 
 def run():
     search_string = ' '.join(sys.argv[1:])
