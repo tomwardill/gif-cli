a5793adb2d85126aa50f2d0e77fbb6bd3baeee28
Tom Wardill <tom@howrandom.net>
2018-10-02 16:24:24 +0100

    Initial commit

---
+++ gifcli/gifcli.py
@@ -0,0 +1,39 @@
+import json
+import os
+import random
+import sys
+
+import requests
+
+
+API_BASE = 'https://api.giphy.com/v1/gifs/search'
+
+
+def get_config():
+    config_dir = os.path.abspath(os.environ.get('SNAP_USER_COMMON', '.'))
+    config_path = os.path.join(config_dir, 'gif-cli.json')
+    with open(config_path) as config_file:
+        config = json.load(config_file)
+
+def run():
+    search_string = ' '.join(sys.argv[1:])
+
+    config = get_config()
+
+    response = requests.get(
+        API_BASE,
+        params={
+            'api_key': config['api_key'],
+            'q': search_string,
+        }
+    )
+
+    if response.status_code != 200:
+        print("Error {} from giphy".format(response.status_code))
+        sys.exit(1)
+
+    payload = response.json()
+    number_in_result = payload['pagination']['count']
+    chosen = random.randint(0, number_in_result - 1)
+    result = payload['data'][chosen]
+    print(result['images']['original']['url'])
---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='gif-cli',
+    version='0.0.1',
+    author='Tom Wardill',
+    author_email='tom@howrandom.net',
+    url="https://github.com/tomwardill/gif-cli",
+    license='LICENSE',
+    description='CLI tool to search for gifs',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+        "requests",
+    ],
+    setup_requires=[
+        "flake8"
+    ],
+    entry_points={
+        'console_scripts': [
+            'gif-cli = gifcli.gifcli:run'
+        ]
+    },
+    packages=setuptools.find_packages(),
+)
