960c433c53f2174677caa5ad45c824c5e01e7d1e
Tom Wardill <tom@howrandom.net>
2018-10-02 17:32:03 +0100

    Use public api key

--- gifcli/gifcli.py
+++ gifcli/gifcli.py
@@ -10,11 +10,14 @@ API_BASE = 'https://api.giphy.com/v1/gifs/search'
 
 
 def get_config():
-    config_dir = os.path.abspath(os.environ.get('SNAP_USER_COMMON', '.'))
-    config_path = os.path.join(config_dir, 'gif-cli.json')
-    with open(config_path) as config_file:
-        config = json.load(config_file)
-        return config
+    try:
+        config_dir = os.path.abspath(os.environ.get('SNAP_USER_COMMON', '.'))
+        config_path = os.path.join(config_dir, 'gif-cli.json')
+        with open(config_path) as config_file:
+            config = json.load(config_file)
+            return config
+    except:
+        return {'api_key': 'dc6zaTOxFJmzC'}
 
 def run():
     search_string = ' '.join(sys.argv[1:])
