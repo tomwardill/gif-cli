4fa2af887fe25449fcf818ecab66fd34003ace86
Tom Wardill <tom@howrandom.net>
2018-10-14 21:13:58 +0100

    Run black and flake8

--- gifcli/gifcli.py
+++ gifcli/gifcli.py
@@ -6,30 +6,27 @@ import sys
 import requests
 
 
-API_BASE = 'https://api.giphy.com/v1/gifs/search'
+API_BASE = "https://api.giphy.com/v1/gifs/search"
 
 
 def get_config():
     try:
-        config_dir = os.path.abspath(os.environ.get('SNAP_USER_COMMON', '.'))
-        config_path = os.path.join(config_dir, 'gif-cli.json')
+        config_dir = os.path.abspath(os.environ.get("SNAP_USER_COMMON", "."))
+        config_path = os.path.join(config_dir, "gif-cli.json")
         with open(config_path) as config_file:
             config = json.load(config_file)
             return config
-    except:
-        return {'api_key': 'dc6zaTOxFJmzC'}
+    except IOError:
+        return {"api_key": "dc6zaTOxFJmzC"}
+
 
 def run():
-    search_string = ' '.join(sys.argv[1:])
+    search_string = " ".join(sys.argv[1:])
 
     config = get_config()
 
     response = requests.get(
-        API_BASE,
-        params={
-            'api_key': config['api_key'],
-            'q': search_string,
-        }
+        API_BASE, params={"api_key": config["api_key"], "q": search_string}
     )
 
     if response.status_code != 200:
@@ -37,7 +34,7 @@ def run():
         sys.exit(1)
 
     payload = response.json()
-    number_in_result = payload['pagination']['count']
+    number_in_result = payload["pagination"]["count"]
     chosen = random.randint(0, number_in_result - 1)
-    result = payload['data'][chosen]
-    print(result['images']['original']['url'])
+    result = payload["data"][chosen]
+    print(result["images"]["original"]["url"])
